27e5f17c287571fcd15cf31d8a596e9224049775
grace <gracechongbl@gmail.com>
2020-12-04 09:32:48 +0800

    changes made

---
+++ screens/EventsScreen.js
@@ -0,0 +1,7 @@
+function Eventsscreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Screens!</Text>
+      </View>
+    );
+  }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,8 @@
+function HomeScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+  
