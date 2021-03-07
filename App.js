b1b29507535bcc1b494c1c17815edc01a0b7b96e
grace <gracechongbl@gmail.com>
2021-03-08 01:09:48 +0800

    added font awesome, imported card, card Item, accordion, array

--- App.js
+++ App.js
@@ -5,13 +5,38 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
 import EventsScreen from "./screens/EventsScreen";
 import ContactScreen from "./screens/ContactScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "ios-home";
+            } else if (route.name === "Events") {
+              iconName = focused ? "ios-list-box" : "ios-list";
+            } else if (route.name === "Contact") {
+              iconName = "ios-people";
+              // iconName = Platform.OS === "ios" ? "ios-people" : "md-people";
+              // return <FontAwesome name="group" size={size - 4} color={color} />;
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "blue",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Events" component={EventsScreen} />
         <Tab.Screen name="Contact" component={ContactScreen} />
